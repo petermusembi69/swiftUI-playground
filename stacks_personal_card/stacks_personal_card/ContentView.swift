c7a4d645e59dfca97c0487809ee4438ab593c220
petermusembi69 <nyongesamusembi@gmail.com>
2022-03-28 18:33:38 +0100

    Inline file refactor

--- stacks_personal_card/stacks_personal_card/ContentView.swift
+++ stacks_personal_card/stacks_personal_card/ContentView.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
+struct CircleImage: View {
+    var imageName: String
+    
+    var body: some View {
+        Image(systemName: imageName)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 80, height: 80).foregroundColor(.blue).overlay(Circle().stroke(.white,lineWidth: 1))
+            .shadow(radius:3)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         HStack{
             
-            Image(systemName: "p.circle.fill")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 80, height: 80).foregroundColor(.blue).overlay(Circle().stroke(.white,lineWidth: 1))
-                .shadow(radius:3)
+            
+            CircleImage(imageName: "p.circle.fill")
             VStack(alignment: .leading)
             {
                 
