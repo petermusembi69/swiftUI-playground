db55728d066a079e2401d2d740c55df50c319d86
petermusembi69 <nyongesamusembi@gmail.com>
2022-03-29 00:38:40 +0100

    Create noTodo app

---
+++ noTodo/noTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  noTodo
+//
+//  Created by user205798 on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}	
---
+++ noTodo/noTodo/noTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  noTodoApp.swift
+//  noTodo
+//
+//  Created by user205798 on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct noTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
