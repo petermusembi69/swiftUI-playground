ad030612dea6d7aee70107bf9dd032e8d544e713
petermusembi69 <nyongesamusembi@gmail.com>
2022-03-28 18:46:01 +0100

    Create project quotes

---
+++ quotes/Shared/quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotesApp.swift
+//  Shared
+//
+//  Created by user205798 on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
