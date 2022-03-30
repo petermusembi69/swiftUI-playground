150cb17b95fd8bc4c886459da1a593179f86241a
petermusembi69 <nyongesamusembi@gmail.com>
2022-03-30 01:39:22 +0100

    Create binding and textField

--- noTodo/noTodo/ContentView.swift
+++ noTodo/noTodo/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var nodo: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    Group {
+                        TextField("What will You NOT Do Today?", text: self.$nodo, onEditingChanged: {(changed) in print(changed)}) {
+                            print("onCommit time")
+                        }
+                    }
+                    }
+                }
+            
+        }.navigationBarTitle(Text("No Todo"))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portraitUpsideDown)
     }
-}	
+}
