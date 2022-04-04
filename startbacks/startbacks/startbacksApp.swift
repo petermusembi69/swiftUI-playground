c6fd48f7d1ef2c3a89f7211bed1798eccf809a6d
petermusembi69 <nyongesamusembi@gmail.com>
2022-04-04 10:20:56 +0100

    Add starbucks app

---
+++ startbacks/startbacks/startbacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  startbacksApp.swift
+//  startbacks
+//
+//  Created by user205798 on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct startbacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
