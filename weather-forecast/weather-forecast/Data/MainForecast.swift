cd74a2d70dc35bd3ad08aed65eb0e2d34b8337b5
petermusembi69 <nyongesamusembi@gmail.com>
2022-04-05 14:42:20 +0100

    Add data models

---
+++ weather-forecast/weather-forecast/Data/MainForecast.swift
@@ -0,0 +1,52 @@
+//
+//  MainForecast.swift
+//  weather-forecast
+//
+//  Created by user205798 on 4/5/22.
+//
+
+import Foundation
+import SwiftUI
+
+class MainForecastResponse: Decodable {
+    var cod: Int?
+    var coor: Coordinates
+    var sys: Sys
+    var main: Main
+    var dt: Int
+    var weather: Weather
+    var clouds: Clouds
+    var wind: Wind
+}
+
+class Wind: Decodable {
+    var speed: Double
+    var deg: Int
+    var gust: Double
+}
+
+class Clouds: Decodable {
+    var all: Int
+}
+
+class Weather: Decodable {
+    var id: Int
+    var main: String
+    var description: String
+    var icon: String
+}
+
+class Main: Decodable {
+    var temp: Double
+    var pressure: Int
+    var humidity: Int
+}
+
+class Coordinates: Decodable {
+    var lon: Double
+    var lat: Double
+}
+
+class Sys: Decodable {
+    var country: String
+}
