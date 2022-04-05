27eeaba60a767751ac8f0ab3d3bd0882deafc1ce
petermusembi69 <nyongesamusembi@gmail.com>
2022-04-05 12:31:31 +0100

    Add weather-forecast app

---
+++ weather-forecast/weather-forecast/weather_forecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_forecastApp.swift
+//  weather-forecast
+//
+//  Created by user205798 on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct weather_forecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
